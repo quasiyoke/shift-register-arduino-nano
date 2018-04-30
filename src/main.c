3b6756e22fef34e13ce139c7aafb8fbee7469446
Petr Ermishkin <quasiyoke@gmail.com>
2018-04-30 12:17:52 +0300

    Shift register with latched output demo was implemented.

--- src/main.c
+++ src/main.c
@@ -4,7 +4,8 @@
 #define BYTE_LENGTH (8)
 #define LED_PIN (PB5)
 #define SERIAL_OUTPUT_PIN (LED_PIN)
-#define CLK_PIN (PB4)
+#define SHIFT_REGISTER_CLK_PIN (PB4)
+#define REGISTER_CLK_PIN (PB3)
 
 const char MESSAGE[] = {
   0b00000000,
@@ -15,10 +16,13 @@ const char MESSAGE[] = {
 
 int main(void) {
   DDRB = _BV(SERIAL_OUTPUT_PIN)
-    | _BV(CLK_PIN);
+    | _BV(SHIFT_REGISTER_CLK_PIN)
+    | _BV(REGISTER_CLK_PIN);
 
   for (int i = 0; ; i = (i + 1) % sizeof(MESSAGE)) {
     char valueByte = MESSAGE[i];
+    // Make a falling edge of storage register clock signal
+    PORTB &= ~(_BV(REGISTER_CLK_PIN));
 
     for (char j = BYTE_LENGTH - 1; j >= 0; --j) {
       char valueBit = (valueByte >> j) & 1;
@@ -26,11 +30,14 @@ int main(void) {
       PORTB &= ~(_BV(SERIAL_OUTPUT_PIN));
       // Send current bit value to serial output
       PORTB |= valueBit << SERIAL_OUTPUT_PIN;
-      // Make a rising edge of clock signal
-      PORTB |= _BV(CLK_PIN);
-      // Make a falling edge of clock signal
-      PORTB &= ~(_BV(CLK_PIN));
-      _delay_ms(250);
+      // Make a rising edge of shift register clock signal
+      PORTB |= _BV(SHIFT_REGISTER_CLK_PIN);
+      // Make a falling edge of shift register clock signal
+      PORTB &= ~(_BV(SHIFT_REGISTER_CLK_PIN));
     }
+
+    // Make a rising edge of storage register clock signal
+    PORTB |= _BV(REGISTER_CLK_PIN);
+    _delay_ms(250);
   }
 }
